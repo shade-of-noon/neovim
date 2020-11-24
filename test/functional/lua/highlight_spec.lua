1c52ad4bd9215f541980ad608a56dca9e59c61cc
Edwin Pujols <edwin.pujols5@outlook.com>
2020-11-24 09:43:54 -0400

    Fix tests and make them run faster.

--- test/functional/lua/highlight_spec.lua
+++ test/functional/lua/highlight_spec.lua
@@ -5,34 +5,30 @@ local command = helpers.command
 local feed = helpers.feed
 
 describe('highlight.on_yank()', function()
-  describe('arguments', function()
-    local screen
-    local enable_hl_on_yank = function(args)
-      higroup = args['higroup'] or 'IncSearch'
-      timeout = args['timeout'] or 150
-      on_macro = args['on_macro'] and 'true' or 'false'
-      on_visual = args['on_visual'] and 'true' or 'false'
-      command('augroup YankHLGroup')
-      command('au TextYankPost * '
-        ..'silent! lua vim.highlight.on_yank {higroup="'..higroup
-        ..'", timeout='..timeout..', on_macro='..on_macro
-        ..', on_visual='..on_visual..'}')
-      command('augroup END')
-    end
+  local screen
+  local HL_TIMEOUT = 30
+  local enable_hl_on_yank = function(args)
+    higroup = args['higroup'] or 'IncSearch'
+    timeout = args['timeout'] or 150
+    on_macro = args['on_macro'] == nil and 'true' or 'false'
+    on_visual = args['on_visual'] == nil and 'true' or 'false'
 
+    command('au TextYankPost * '
+      ..'silent! lua vim.highlight.on_yank {higroup="'..higroup
+      ..'", timeout='..timeout..', on_macro='..on_macro
+      ..', on_visual='..on_visual..'}')
+  end
+
+  describe('arguments', function()
     before_each(function()
       clear()
-      screen = Screen.new(20,1)
+      screen = Screen.new(20, 1)
       screen:attach()
       screen:set_default_attr_ids( {
         [0] = {reverse=true},
         [1] = {italic=true}
       } )
       feed('iuna línea solamente<esc>')
-    end)
-
-    after_each(function()
-      command('au! YankHLGroup')
     end)
 
     it('works with non-default higroup', function()
@@ -42,106 +38,113 @@ describe('highlight.on_yank()', function()
       screen:expect{grid=[[
         {1:una línea solament^e} |
                             |
-      ]], timeout=100}
+      ]]}
     end)
 
     it('works with non-default timeout', function()
-      enable_hl_on_yank {timeout=330}
+      enable_hl_on_yank {timeout=HL_TIMEOUT}
       feed('yy')
       screen:expect{grid=[[
         {0:una línea solament^e} |
                             |
-      ]], timeout=100}
+      ]]}
 
       feed('yy')
       screen:expect{grid=[[
         una línea solament^e |
                             |
-      ]], timeout=330}
+      ]], timeout=HL_TIMEOUT}
     end)
 
-    -- FIXME: Why doesn't this work?
-    pending('ignores visual yanks when on_visual=false', function()
-      enable_hl_on_yank {on_visual=false}
-
+    it('ignores visual yanks when on_visual=false', function()
+      enable_hl_on_yank {on_visual=false, timeout=HL_TIMEOUT}
       -- sanity check
       feed('yy')
       screen:expect{grid=[[
         {0:una línea solament^e} |
                             |
-      ]], timeout=100}
+      ]]}
+
+      -- wait for sanity check to clear
+      screen:expect{grid=[[
+        una línea solament^e |
+                            |
+      ]], timeout=HL_TIMEOUT}
 
       feed('Vy')
       screen:expect{grid=[[
         ^una línea solamente |
                             |
-      ]], timeout=100}
+      ]]}
     end)
 
     it('ignores in-macro yanks when on_macro=false', function()
-      enable_hl_on_yank {on_macro=false,timeout=150}
-
+      enable_hl_on_yank {on_macro=false, timeout=HL_TIMEOUT}
       -- sanity check
       feed('yy')
       screen:expect{grid=[[
         {0:una línea solament^e} |
                             |
-      ]], timeout=100}
+      ]]}
 
-      feed('0qqyiwwq@q')
+      -- create macro
+      feed('0qqyiwwq')
+
+      -- wait for macro to clear
       screen:expect{grid=[[
-        una línea ^solamente |
+        una ^línea solamente |
                             |
-      ]], timeout=200}
+      ]], timeout=HL_TIMEOUT}
+
+      feed('@qv4lp')
+      screen:expect{grid=[[
+        una línea líne^aente |
+                            |
+      ]]}
     end)
   end)
 
   describe('when virtualedit=all', function()
-    local screen
     before_each(function()
       clear()
-      screen = Screen.new(25,5)
+      screen = Screen.new(25, 5)
       screen:attach()
       screen:set_default_attr_ids( {
         [0] = {reverse=true},
         [1] = {bold=true, foreground=Screen.colors.Blue},
       } )
       command('set virtualedit=all')
-      command(string.gsub([[
-        au TextYankPost *
-         silent! lua vim.highlight.on_yank {
-           timeout=250
-         }]], '\n', ' '))
       feed('ishórt<cr>hint: a loonger line<cr>galỉłeö<esc>')
+      enable_hl_on_yank {timeout=HL_TIMEOUT}
     end)
 
     it('block region is highlighted correctly', function()
-      feed('gg0ljy')
+      feed('gg0<c-v>ljy')
       screen:expect{grid=[[
         {0:^sh}órt                    |
         {0:hi}nt: a loonger line     |
         galỉłeö                  |
         {1:~                        }|
                                  |
-      ]], timeout=100}
+      ]]}
 
-      feed('2l3j12ly')
+      feed('2l<c-v>3j12ly')
       screen:expect{grid=[[
         sh{0:^órt}                    |
         hi{0:nt: a loonger} line     |
         ga{0:lỉłeö}                  |
         {1:~                        }|
         block of 3 lines yanked  |
-      ]], timeout=100}
+      ]]}
 
-      feed('5l2j6ly')
+      feed('5l<c-v>2j6ly')
       screen:expect{grid=[[
         shórt  ^                  |
         hint: a {0:loonger} line     |
         galỉłeö                  |
         {1:~                        }|
         block of 3 lines yanked  |
-      ]], timeout=100}
+      ]]}
     end)
 
     it('yy highlights whole lines', function()
@@ -152,7 +155,7 @@ describe('highlight.on_yank()', function()
         {0:galỉłe^ö}                  |
         {1:~                        }|
                                  |
-      ]], timeout=100}
+      ]]}
 
       feed('5lyy')
       screen:expect{grid=[[
@@ -161,7 +164,7 @@ describe('highlight.on_yank()', function()
         {0:galỉłeö}    ^              |
         {1:~                        }|
                                  |
-      ]], timeout=100}
+      ]]}
 
       feed('gg20|3yy')
       screen:expect{grid=[[
@@ -170,7 +173,7 @@ describe('highlight.on_yank()', function()
         {0:galỉłeö}                  |
         {1:~                        }|
         3 lines yanked           |
-      ]], timeout=100}
+      ]]}
     end)
   end)
 end)
